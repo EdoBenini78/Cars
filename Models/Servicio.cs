f88404d25501e0ee2925bc1ac8760b26e186a321
Karina Hessdorfer <karihessdorfer@hotmail.com>
2019-11-16 15:05:10 -0300

    df

--- Models/Servicio.cs
+++ Models/Servicio.cs
@@ -1,6 +1,7 @@
 ﻿using CARS.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace CARS.Models
 {
@@ -12,20 +13,21 @@ namespace CARS.Models
         public Taller Taller { get; set; }
         public TipoServicio Tipo{ get; set; }
         public DateTime FechaSugerida { get; set; }
-        public DateTime FechaEntrata { get; set; }
+        
+        [DisplayName("Fecha de entrada")]public DateTime FechaEntrada { get; set; }
         public DateTime FechaSalida { get; set; }
         public TipoEstado Estado{ get; set; }
         public List<Factura> Facturas { get; set; }
         #endregion
 
         #region Constructor
-        public Servicio(Vehiculo vehiculo, Taller taller, TipoServicio tipo, DateTime fechaSugerida, DateTime fechaEntrata, DateTime fechaSalida, TipoEstado estado, List<Factura> facturas)
+        public Servicio(Vehiculo vehiculo, Taller taller, TipoServicio tipo, DateTime fechaSugerida, DateTime fechaEntrada, DateTime fechaSalida, TipoEstado estado, List<Factura> facturas)
         {
             Vehiculo = vehiculo;
             Taller = taller;
             Tipo = tipo;
             FechaSugerida = fechaSugerida;
-            FechaEntrata = fechaEntrata;
+            FechaEntrada = fechaEntrada;
             FechaSalida = fechaSalida;
             Estado = estado;
             Facturas = facturas;
